0cbd9172881374fe00c5d85eeda308b573fc2002
AndreaColombo <andrea.colombo.897@gmail.com>
2016-05-17 10:42:45 +0200

    [CODE] Minor error fix

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
-#include <thread>
 
 #include "miosix.h"
 #include "adc.h"
@@ -10,27 +9,31 @@ using namespace std;
 using namespace miosix;
 
 int x;
-Typedef gpio<GPIOA_BASE,0> recalibratingButton;
-Typedef gpio<GPIOD_BASE,12> green;
-Typedef gpio<GPIOD_BASE,13> orange;
-Typedef gpio<GPIOD_BASE,14> blue;
-Typedef gpio<GPIOD_BASE,15> red;
+typedef Gpio<GPIOA_BASE,0> recalibratingButton;
+typedef Gpio<GPIOD_BASE,12> green;
+typedef Gpio<GPIOD_BASE,13> orange;
+typedef Gpio<GPIOD_BASE,14> blue;
+typedef Gpio<GPIOD_BASE,15> red;
 Adc adc;
 
 void blinkLed (){ //blinka i led a giro mentre ricalibra penso sia figo
-	duration = 1000000; //per ora la hardcodo
-	for(int i = 0; i<duration; i++){
+	green::mode(Mode::OUTPUT);
+	blue::mode(Mode::OUTPUT);
+	red::mode(Mode::OUTPUT);
+	orange	::mode(Mode::OUTPUT);
+	int duration = 1000000; //per ora la hardcodo
+	for(int i = 0; i<1000000; i++){
 		green::high();
-		Thread:sleep(10);
+		Thread::sleep(10);
 		green::low();
 		orange::high();
 		Thread:sleep(10);
 		orange::low();
 		blue::high();
-		Thread:sleep(10);
+		Thread::sleep(10);
 		blue::low();
 		red::high();
-		Thread:sleep(10);
+		Thread::sleep(10);
 		red::low();
 	}
 }
@@ -43,7 +46,7 @@ void recalibration (){
 	thread t2(blinkLed);
 	for (int i = 0; i<1000000; i++){
 		//qua devo bloccare l'esecuzione del main finchè non si ricalibra
-		x = adc.redd();
+		x = adc.read();
 	}
 	 
 	  
