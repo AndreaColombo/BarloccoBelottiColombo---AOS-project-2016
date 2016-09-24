b718f70ad315ad4c7cca94438ee672f2a7a2a773
AndreaColombo <andrea.colombo.897@gmail.com>
2016-09-24 14:42:50 +0200

    Main cambiato e grafici

--- main.cpp
+++ main.cpp
@@ -10,60 +10,28 @@ using namespace miosix;
 #define BUFF 30000
 #define NUM0 215
 #define NUM1 205
-
-int x;
-typedef Gpio<GPIOA_BASE,0> recalibratingButton;
-typedef Gpio<GPIOD_BASE,12> green;
-typedef Gpio<GPIOD_BASE,13> orange;
-typedef Gpio<GPIOD_BASE,14> blue;
-typedef Gpio<GPIOD_BASE,15> red;
+#define THRESHOLD 370
 
 Adc adc;
 short buffer[BUFF];
 
 int main(){
 	short tmp;
-	int count1=0,count0=0,bit;
-	char code[100];
-	for(int k=0;k<100;k++)
-		code[k]='2';
+	
 	while(1){
 		adc.start();
 		tmp=adc.read();
 		//cout << tmp << endl;
-		if(tmp>380){
+		if(tmp>THRESHOLD){
 			for(int i=0;i<BUFF;i++){
 				adc.start();
-				buffer[i]=adc.read();
+				tmp = adc.read();
+				if (tmp > THRESHOLD+50)
+					buffer[i]=1;
+				else buffer[i]=0;
 			}
-			for(int i=0,k=0;i<BUFF;i++){
-				if(buffer[i]>380){
-					count1++;
-					if(count0>0){
-						cout << count0 << " blocco di 0" << endl;
-						bit=(count0/NUM0)-1;
-						for(int j=0;j<bit;j++){
-							code[k]='0';
-							k++;
-						}
-						count0=0;
-					}
-				}
-				else{
-					count0++;
-					if(count1>0){
-						cout << count1 << " blocco di 1" << endl;
-						bit=(count1/NUM1);
-						for(int j=0;j<bit;j++){
-							code[k]='1';
-							k++;
-						}
-						count1=0;
-					}
-				}
-			}
-			for(int k=0;code[k]!='2';k++)
-				cout << code[k] << endl;
+			for(int i =0; i<BUFF; i++)
+				cout << buffer[i] << endl;
 		}
 	}
 }
